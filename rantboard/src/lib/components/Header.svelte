41975137fa9f850cb2f0ea441f5806cf208a6582
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-08-03 15:51:49 +0800

    Design Header

--- rantboard/src/lib/components/Header.svelte
+++ rantboard/src/lib/components/Header.svelte
@@ -1,6 +1,42 @@
 <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
+    <div>
+        <img src="/images/logo.png" alt="Rantboard Logo" class="logo"/>
+    </div>
+    <div class="pages">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </div>
 </nav>
 
+<style>
+    nav {
+        background-color: var(--defaultGray);
+        height: 86px;
+        font-size: 20px;
+        font-weight: 700;
+        display: grid;
+        grid-template-columns: 60% 40%;
+        grid-template-rows: 100%; 
+        color: var(--black);
+    }
 
+    .pages {
+        display: flex;
+        align-items: center;
+        gap: 50px;
+        border: 1px solid black;
+        width: 100%;
+        height: 100%;
+    }
+
+    nav > div:first-child {
+        display: flex;
+    }
+
+    .logo {
+        height: 36px; 
+        align-self: center;
+        padding-left: 95px;
+    }
+</style>
--- rantboard/src/routes/+layout.svelte
+++ rantboard/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import Header from "$lib/components/Header.svelte";
     import Footer from "$lib/components/Footer.svelte";
     import '../styles.css';
+    import '@fontsource-variable/plus-jakarta-sans';
 </script>
 
 <Header />
